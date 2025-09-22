5bee2c4fe805c352d58285962cf694f15f7da51e
Rohan Ul Haq <rohanulhaq004@gmail.com>
2025-09-22 17:48:10 +0500

    feat: add Estenarh E2E test suite and update wdio configuration to include new test file

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -12,7 +12,7 @@ export const config: any = {
     // new commit 
     port: 4723,
     specs: [
-        './test/specs/Test1.ts'
+        '.test/specs/estenarhE2E.ts'
     ],
     exclude: [],
     maxInstances: 10,
